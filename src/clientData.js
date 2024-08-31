a960fef26f1106d97a5576352db05cfe595f0de9
Abhishek Ghodke <abhighodke3@gmail.com>
2024-08-31 13:35:17 +0530

    client updated

--- src/clientData.js
+++ src/clientData.js
@@ -1,37 +1,31 @@
 // src/clientData.js
 const clientData = {
-    headerInitials: "AS", // Header Initials
-    heroTagline: "Allen Snow Realty", // Hero Tagline
-    agentName: "Allen Snow",
-    aboutMeShort: "The son of a nuclear physicist, Allen Snow was raised in the historic small-town of Oak Ridge, Tennessee ('The Atomic City'), after which he attended Auburn University where he obtained a bachelor's degree in civil engineering, unique amongst REALTORS®. While working as an engineer for 10 years before switching to real estate sales in 2001, Allen spent time in many areas of the country including cities such as Atlanta, Boston, Fort Lauderdale, Memphis, Miami, Raleigh, Providence and San Francisco. Each time Allen visited Atlanta, he felt a deep connection to the City where he ultimately found both business and personal success.", // AboutMe Short Description
-    aboutMeDetailed: `The son of a nuclear physicist, Allen Snow was raised in the historic small-town of Oak Ridge, Tennessee ('The Atomic City'), after which he attended Auburn University where he obtained a bachelor's degree in civil engineering, unique amongst REALTORS®. While working as an engineer for 10 years before switching to real estate sales in 2001, Allen spent time in many areas of the country including cities such as Atlanta, Boston, Fort Lauderdale, Memphis, Miami, Raleigh, Providence and San Francisco. Each time Allen visited Atlanta, he felt a deep connection to the City where he ultimately found both business and personal success. Allen has now resided in Midtown for over 17 years where he enjoys the outdoors in Piedmont Park, bikes the Atlanta BeltLine, and embraces the diverse culture and fantastic local cuisine. Allen's pride, passion and enthusiasm for the city of Atlanta is reflected in his successful real estate business. 'Every time I open the Atlanta Business Chronicle, an exciting new project is being planned, and I love sharing that information with my clients and getting them as excited about it as I am,' Allen says.
+    headerInitials: "CC",
+    heroTagline: "Cassie Carter Realty",
+    agentName: "Cassie Carter",
+    aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.",
+    aboutMeDetailed: `It is my passion to assist clients with reaching their goal of owning a home or selling their current home to find a new one. It is important to me that my clients find the ideal home to match their needs and their budget. Also, I have helped many investors find the perfect property to complete their investment portfolio. 
 
-Since 2001, the year Allen decided to delve into real estate sales full time, he began earning notable awards such as 'Rookie of the Year,' 'Agent of the Year' nearly every year, ranking in the top 1% among 50,000 real estate agents internationally, and ranking No. 3 in Metro Atlanta New Home Sales category in 2014. Currently, he is on track to meet or exceed these same goals for 2015. Allen owes his success to loving what he does, building relationships and working well with people.
+With my knowledge and experience I provide my clients accurate and complete detailed information so they can make the best decisions. Also, with being certified to help our Veterans and their families in finding a home and utilizing their full VA eligibility. Allow me to assist you with your real estate needs.
 
-It was with Allen's degree and 10 years of experience in civil engineering that his interest and insight in real estate sales and real estate development grew and he began leveraging that into individual buyer and seller services. Over the years, Allen has expanded his portfolio by working with reputable developers, such as Minerva Homes, Cablik Enterprises, Daniel Corporation and Jamestown, to mention a few. Allen's experience and ever-expanding skill set gives him the tools to assist individual buyers and sellers through the challenging process of buying and selling their home, but also the unique tools to help developers navigate the complexities of new home residential project sales and sales management.
-
-Today, Allen Snow Realty is working with Atlanta Fine Homes Sotheby's International Realty and is continuing to build upon the new energy that has taken the city of Atlanta by storm.`, // AboutMe Detailed Description
-    testimonials: [
-        { quote: "Working with Allen was a game-changer for us. His deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", author: "John and Emily R." },
-        { quote: "Allen Snow Realty provided exceptional service from start to finish. The team’s professionalism and attention to detail ensured that our property sale was smooth and successful. We couldn’t have asked for a better partner in real estate.", author: "Michael S." },
-        { quote: "The dedication and expertise of Allen were evident throughout our home-buying journey. His ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", author: "Sarah and David L." },
-        { quote: "Allen and his team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", author: "Jessica and Mark T." },
-        { quote: "If you’re looking for a real estate agent who goes above and beyond, Allen Snow is the one. His dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", author: "Laura K." }
-    ], // Testimonials
-    footerTagline: "Your Trusted Partner in Luxury Real Estate", // Footer Tagline
-    email: "allensnowrealty@gmail.com", // Email
-    address: "1555 Peachtree Street NE, Suite 100, Atlanta, Georgia, 30309, United States", // Address
-    phone: "404-931-1176", // Phone Number
-    footerDescription: "Allen Snow Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.", // Footer Description
+Customer Service excellence is our Passion!`,
+    testimonials: [{"quote": "Working with Cassie Carter was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Cassie Carter provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Cassie Carter were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Cassie Carter and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Cassie Carter is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
+    footerTagline: "Your Trusted Partner in Luxury Real Estate",
+    email: "",
+    address: "Birmingham, AL, USA",
+    phone: "205-540-4214",
+    footerDescription: `Cassie Carter Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
     socialMediaLinks: {
-        facebook: "",
+        facebook: "https://www.facebook.com/cassie.carter.581525/",
         Instagram: "",
         linkedin: "",
-    }, // Social Media Links
+    },
     contactDetails: {
-        email: "allensnowrealty@gmail.com",
-        phone: "404-931-1176",
-    }, // Contact Details
+        email: "",
+        phone: "205-540-4214",
+    },
+    website: "",
+    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/449460641_3722791247957771_4935030469472823776_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=sF7sYipEQRcQ7kNvgEjOK0E&_nc_ht=scontent-sjc3-1.xx&_nc_gid=A-bQCc-AW5KvzQ1493gabcd&oh=00_AYDbtVZtmdp05_ZrWlcWjr-nuXFiMsYAblctKmviNcCIfA&oe=66D7D5DF" // Agent image URL
 };
 
 export default clientData;
