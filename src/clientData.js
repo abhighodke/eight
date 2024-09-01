8dbd096ef7de4ee924dac1a6d6f147d585ca72a4
Abhishek Ghodke <abhighodke3@gmail.com>
2024-09-01 20:12:11 +0530

    Changes

--- src/clientData.js
+++ src/clientData.js
@@ -9,23 +9,23 @@ const clientData = {
 With my knowledge and experience I provide my clients accurate and complete detailed information so they can make the best decisions. Also, with being certified to help our Veterans and their families in finding a home and utilizing their full VA eligibility. Allow me to assist you with your real estate needs.
 
 Customer Service excellence is our Passion!`,
-    testimonials: [{"quote": "Working with Cassie Carter was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Cassie Carter provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Cassie Carter were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Cassie Carter and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Cassie Carter is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
+    testimonials: [{ "quote": "Working with Cassie Carter was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R." }, { "quote": "Cassie Carter provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S." }, { "quote": "The dedication and expertise of Cassie Carter were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L." }, { "quote": "Cassie Carter and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T." }, { "quote": "If you are looking for a real estate agent who goes above and beyond, Cassie Carter is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K." }],
     footerTagline: "Your Trusted Partner in  Real Estate.",
-    email: "",
-    address: "Birmingham, AL, USA",
+    email: "cassiecarterrealtor@gmail.com",
+    address: "2964 Columbiana Rd, Birmingham, AL, USA",
     phone: "205-540-4214",
     footerDescription: `Cassie Carter Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
     socialMediaLinks: {
-        facebook: "https://www.facebook.com/cassie.carter.581525/",
+        facebook: "https://www.facebook.com/cassiesellsyourhome/",
         Instagram: "",
         linkedin: "",
     },
     contactDetails: {
-        email: "",
+        email: "cassiecarterrealtor@gmail.com",
         phone: "205-540-4214",
     },
     website: "",
-    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/449460641_3722791247957771_4935030469472823776_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=sF7sYipEQRcQ7kNvgEjOK0E&_nc_ht=scontent-sjc3-1.xx&_nc_gid=A-bQCc-AW5KvzQ1493gabcd&oh=00_AYDbtVZtmdp05_ZrWlcWjr-nuXFiMsYAblctKmviNcCIfA&oe=66D7D5DF" // Agent image URL
+    dp: "https://scontent.fpnq17-1.fna.fbcdn.net/v/t39.30808-6/273383670_316098860536990_4355348625341332987_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=XdNbUpBaM8EQ7kNvgHwaTGj&_nc_ht=scontent.fpnq17-1.fna&oh=00_AYA51hBwV6IYW2U8F_MBYoXw3uHMLYQfoo_zoYselOhb_A&oe=66DA349A" // Agent image URL
 };
 
 export default clientData;
