ef3214a476567449a5b591efad3e8d89b736a1cd
Abhishek Ghodke <abhighodke3@gmail.com>
2024-09-04 21:37:45 +0530

    client updated

--- src/clientData.js
+++ src/clientData.js
@@ -1,31 +1,31 @@
 // src/clientData.js
 const clientData = {
-    headerInitials: "CC",
-    heroTagline: "Cassie Carter Realty",
-    agentName: "Cassie Carter",
+    headerInitials: "ST",
+    heroTagline: "Stephanie Taylor Realty",
+    agentName: "Stephanie Taylor",
     aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.",
     aboutMeDetailed: `It is my passion to assist clients with reaching their goal of owning a home or selling their current home to find a new one. It is important to me that my clients find the ideal home to match their needs and their budget. Also, I have helped many investors find the perfect property to complete their investment portfolio. 
 
 With my knowledge and experience I provide my clients accurate and complete detailed information so they can make the best decisions. Also, with being certified to help our Veterans and their families in finding a home and utilizing their full VA eligibility. Allow me to assist you with your real estate needs.
 
 Customer Service excellence is our Passion!`,
-    testimonials: [{"quote": "Working with Cassie Carter was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Cassie Carter provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Cassie Carter were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Cassie Carter and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Cassie Carter is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
+    testimonials: [{"quote": "Working with Stephanie Taylor was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Stephanie Taylor provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Stephanie Taylor were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Stephanie Taylor and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Stephanie Taylor is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
     footerTagline: "Your Trusted Partner in Luxury Real Estate",
-    email: "cassiecarterrealtor@gmail.com",
-    address: "2964 Columbiana Rd, Birmingham, AL, USA",
-    phone: "205-540-4214",
-    footerDescription: `Cassie Carter Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
+    email: "stephanierealtygirl@gmail.com",
+    address: "319-C 1st Ave East \nOneonta, AL 35121",
+    phone: "(205) 369-0912",
+    footerDescription: `Stephanie Taylor Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
     socialMediaLinks: {
-        facebook: "https://www.facebook.com/cassiesellsyourhome/",
+        facebook: "https://www.facebook.com/saphiltaylor/",
         Instagram: "",
         linkedin: "",
     },
     contactDetails: {
-        email: "cassiecarterrealtor@gmail.com",
-        phone: "205-540-4214",
+        email: "stephanierealtygirl@gmail.com",
+        phone: "(205) 369-0912",
     },
     website: "",
-    dp: "https://scontent.fpnq17-1.fna.fbcdn.net/v/t39.30808-6/273383670_316098860536990_4355348625341332987_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=XdNbUpBaM8EQ7kNvgHwaTGj&_nc_ht=scontent.fpnq17-1.fna&oh=00_AYDTtEuo9cUx7jeBysEmgcpeKXesQaCNUrE-T8zZODx85A&oe=66DA6CDA" // Agent image URL
+    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/428624357_25498037649783592_2644885744637936035_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=3tVOXypd3zkQ7kNvgE7QRtX&_nc_ht=scontent-sjc3-1.xx&oh=00_AYDIWV5egea0SExnzIFa4gm6w3hM707e7pO-mRSEnmkW6g&oe=66DBCED8" // Agent image URL
 };
 
 export default clientData;
